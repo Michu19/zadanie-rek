85dc1e238759fca1a62fdf8e195157a67a9a1f7c
Michał Szymański <michal.szymanski@softable.pl>
2019-10-28 18:11:54 +0100

    Updates

---
+++ precache-manifest.fec89ae775feb1af93c4ac12f7721355.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c5fb5452a8be1249e060f39388291ba",
+    "url": "/zadanie-rek/index.html"
+  },
+  {
+    "revision": "7ccd7ce2088a58ac73bc",
+    "url": "/zadanie-rek/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7c4d39177206ada6e026",
+    "url": "/zadanie-rek/static/js/2.6682f26f.chunk.js"
+  },
+  {
+    "revision": "7ccd7ce2088a58ac73bc",
+    "url": "/zadanie-rek/static/js/main.6543a157.chunk.js"
+  },
+  {
+    "revision": "fd994bb86953cf3371a9",
+    "url": "/zadanie-rek/static/js/runtime-main.4df28dac.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/zadanie-rek/static/media/logo.25bf045c.svg"
+  }
+]);
